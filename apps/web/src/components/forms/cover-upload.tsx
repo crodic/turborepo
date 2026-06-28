ee074c4b544afb554a343f781577a5b30f32dfe6
phat.bien <phat.bien@manifera.com>
2026-06-28 08:46:16 +0000

    feat: rename folders

--- apps/web/src/components/forms/cover-upload.tsx
+++ apps/web/src/components/forms/cover-upload.tsx
@@ -23,6 +23,8 @@ import { Button } from '@/components/radix-ui/button'
 
 /* eslint-disable @next/next/no-img-element */
 
+/* eslint-disable @next/next/no-img-element */
+
 type CoverPosition = {
   x: number
   y: number
